9d406bf71d5b9e617ee0511e614b29ee93a21bdb
Deepika Joshi <deepika.joshi1@ibm.com>
2025-08-29 19:15:45 +0530

    Created a config file

---
+++ config/db_config.py
@@ -0,0 +1,7 @@
+﻿POSTGRES_CONFIG = {
+    "dbname": "nl2sql_chatbot",
+    "user": "postgres",
+    "password": "ria@1234",  # replace with your actual password
+    "host": "localhost",
+    "port": 5432
+}
